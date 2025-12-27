71c4c9bc79e49ad4d68ea74c75bd929a28b030df
mmellows1 <mmellows1@gmail.com>
2025-12-27 13:15:34 +0000

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'christiejelyoga',
+
+  projectId: 'o854dk5q',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
